d4f4e1626cd584289bca93170d2b0e8dc4dce967
David <david.martin.carl@gmail.com>
2019-02-18 10:58:11 +0100

    Small name changes

--- Assignment_02_18/test_cust.py
+++ Assignment_02_18/test_cust.py
@@ -1,39 +1,39 @@
 import unittest
-import main
+import main_cust
 
 
 class TestStringMethods(unittest.TestCase):
 
     def test_case_1(self):
-        response = main.discount(True, True, True)
+        response = main_cust.discount(True, True, True)
         self.assertEqual(response, None)
 
     def test_case_2(self):
-        response = main.discount(True, True, False)
+        response = main_cust.discount(True, True, False)
         self.assertEqual(response, None)
 
     def test_case_3(self):
-        response = main.discount(True, False, True)
+        response = main_cust.discount(True, False, True)
         self.assertEqual(response, 20)
 
     def test_case_4(self):
-        response = main.discount(True, False, False)
+        response = main_cust.discount(True, False, False)
         self.assertEqual(response, 15)
 
     def test_case_5(self):
-        response = main.discount(False, True, True)
+        response = main_cust.discount(False, True, True)
         self.assertEqual(response, 30)
 
     def test_case_6(self):
-        response = main.discount(False, True, False)
+        response = main_cust.discount(False, True, False)
         self.assertEqual(response, 10)
 
     def test_case_7(self):
-        response = main.discount(False, False, True)
+        response = main_cust.discount(False, False, True)
         self.assertEqual(response, 20)
 
     def test_case_8(self):
-        response = main.discount(False, False, False)
+        response = main_cust.discount(False, False, False)
         self.assertEqual(response, 0)
 
 if __name__ == '__main__':
